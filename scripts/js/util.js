af55e32191c8421e7324ce82cf8e2ad76030c33f
Michael Valdron <michael.valdron@gmail.com>
2020-04-13 10:42:54 -0400

    More util functions added.

--- scripts/js/util.js
+++ scripts/js/util.js
@@ -10,3 +10,29 @@ function clone(obj) {
     
     return copy;
 }
+
+// ref: https://stackoverflow.com/questions/4994201/is-object-empty
+function isEmpty(obj) {
+    // Speed up calls to hasOwnProperty
+    let hasOwnProperty = Object.prototype.hasOwnProperty;
+
+    if (!obj || obj == null) return true;
+
+    if (obj.length > 0)    return false;
+    if (obj.length === 0)  return true;
+
+    if (typeof obj !== "object") return true;
+
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+
+    return true;
+}
+
+function capitalize(str) {
+    let firstChar = str.charAt(0).toUpperCase();
+    let rest = str.substr(1, str.length-1)
+
+    return firstChar + rest;
+}
